96cf9b70a0025bf507d2a5eeb5fe5f01c4cd3fa2
yjhsieh2022 <yijhenhsieh@gmail.com>
2023-06-26 06:35:27 +0000

    added test code

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -10,14 +10,15 @@ import logging
 from unittest import TestCase
 from unittest.mock import MagicMock, patch
 from service import app
-from service.models import db
+from service.models import db, Order, init_db
 from service.common import status  # HTTP Status Codes
 
+BASE_URL = "/orders"
 
 ######################################################################
 #  T E S T   C A S E S
 ######################################################################
-class TestYourResourceServer(TestCase):
+class TestOrderServer(TestCase):
     """ REST API Server Tests """
 
     @classmethod
@@ -27,19 +28,80 @@ class TestYourResourceServer(TestCase):
     @classmethod
     def tearDownClass(cls):
         """ This runs once after the entire test suite """
+        app.config["TESTING"] = True
+        app.config["DEBUG"] = False
+        app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URI
+        app.logger.setLevel(logging.CRITICAL)
+        init_db(app)
 
     def setUp(self):
         """ This runs before each test """
         self.client = app.test_client()
+        db.session.query(Order).delete()  # clean up the last tests
+        db.session.commit()
 
     def tearDown(self):
         """ This runs after each test """
+        db.session.remove()
+
 
     ######################################################################
-    #  P L A C E   T E S T   C A S E S   H E R E
+    #  C A S E S   H E R E
     ######################################################################
 
     def test_index(self):
         """ It should call the home page """
         resp = self.client.get("/")
         self.assertEqual(resp.status_code, status.HTTP_200_OK)
+
+    ######################################################################
+    #  TESTS FOR READ ORDER
+    ######################################################################
+    def test_get_order(self):
+        """It should Read a single Order"""
+        # get the id of an order
+        test_order = self._create_orders(1)[0]
+        response = self.client.get(
+            f"{BASE_URL}/{test_order.id}", content_type="application/json"
+        )
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        data = response.get_json()
+        self.assertEqual(data["id"], test_order.id)
+
+    def test_get_order_not_found(self):
+        """It should not Read an Order that is not found"""
+        response = self.client.get(f"{BASE_URL}/0")
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+
+    ######################################################################
+    #  TESTS FOR READ ITEM
+    ######################################################################
+
+    def test_get_item(self):
+        """It should Read an item from an order"""
+
+        test_order = self._create_orders(1)[0]
+        item = ItemFactory()
+        response = self.client.post(
+            f"{BASE_URL}/{test_order.id}/items",
+            json=item.serialize(),
+            content_type="application/json",
+        )
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+        data = response.get_json()
+        logging.debug(data)
+        item_id = data["id"]
+
+        #  retrieve it back
+        response = self.client.get(
+            f"{BASE_URL}/{order.id}/items/{item_id}",
+            content_type="application/json",
+        )
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        data = response.get_json()
+        logging.debug(data)
+        self.assertEqual(data["order_id"], test_order.id)
+        self.assertEqual(data["id"], item.id)
