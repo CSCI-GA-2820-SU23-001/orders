29cb8f0c53b0ee50fc7bec424ee1ff8f90bd636f
un-poco <chenyingli@chenyinglideMacBook-Air.local>
2023-06-27 21:12:42 -0700

    intial working version of update orders & update items api

--- service/routes.py
+++ service/routes.py
@@ -46,3 +46,44 @@ def create_order():
     res = order.serialize()
     location_url = url_for("read_orders", order_id = order.id, _external = True)
     return jsonify(res), status.HTTP_201_CREATED,{"Location": location_url}
+
+######################################################################
+#  UPDATE AN ORDER
+######################################################################
+
+@app.route('/orders/<int:order_id>', methods=['PUT'])
+def update_order(order_id):
+    app.logger.info("Request to list all orders")
+
+    order = Order.find(order_id)
+    if not order:
+        abort(status.HTTP_404_NOT_FOUND, f"Order with id '{order_id}' does not exist.")
+
+    # Update other fields as neededs
+    data = request.get_json()
+    order.deserialize(data)
+    order.update()
+
+    res = order.serialize()
+    return jsonify(res), status.HTTP_200_OK
+
+######################################################################
+# UPDATE AN ITEM
+######################################################################
+
+@app.route("/orders/<int:order_id>/items/<int:item_id>", methods=["DELETE"])
+def update_items(order_id, item_id):
+    app.logger.info("Request to update Item %s for Order id: %s", item_id, order_id)
+
+    item = Item.find(item_id)
+    if not item:
+        abort(status.HTTP_404_NOT_FOUND, f"Item with id '{item_id}' does not exist.")
+
+    data = request.get_json()
+    item.deserialize(data)
+    item.update()
+
+    res = item.serialize()
+    return jsonify(res), status.HTTP_200_OK
+
+
