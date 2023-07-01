2b122e70a5da70eb74d0da9f825d8b2f777998f6
PepperandSummer <xiaoxuan2097@gmail.com>
2023-07-01 13:43:58 -0400

    Updated error handler in test_routes.py

--- service/routes.py
+++ service/routes.py
@@ -211,3 +211,19 @@ def delete_items(order_id, item_id):
         address.delete()
 
     return make_response("", status.HTTP_204_NO_CONTENT)
+
+######################################################################
+# Helper function
+######################################################################
+
+def check_content_type(media_type):
+    """Checks that the media type is correct"""
+    content_type = request.headers.get("Content-Type")
+    if content_type and content_type == media_type:
+        return
+    app.logger.error("Invalid Content-Type: %s", content_type)
+    abort(
+        status.HTTP_415_UNSUPPORTED_MEDIA_TYPE,
+        f"Content-Type must be {media_type}",
+    )
+
